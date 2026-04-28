587d4a2b7c65a1c7b49b6ebbf8f49bbfabcf08ea
tini <tini67837@gmail.com>
2026-04-28 07:51:34 +0700

    submit modul-5

--- modul-5/src/main.js
+++ modul-5/src/main.js
@@ -1,1 +1,17 @@
 import "./style.css"
+// main.js
+import './style.css'
+// Dark mode toggle
+const btn = document.getElementById('toggleDark')
+const html = document.documentElement
+// Cek preferensi tersimpan
+if (localStorage.getItem('theme') === 'dark') {
+html.classList.add('dark')
+btn.textContent = 'light'
+}
+btn.addEventListener('click', () => {
+    html.classList.toggle('dark')
+const isDark = html.classList.contains('dark')
+btn.textContent = isDark ? 'light' : 'dark'
+localStorage.setItem('theme', isDark ? 'dark' : 'light')
+})
